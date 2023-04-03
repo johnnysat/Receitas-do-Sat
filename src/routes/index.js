141924f3b38630cb440fb26c0d2bf8648d922e44
Johnny <johnny_s.o@hotmail.com>
2023-04-03 18:39:41 -0300

    Adicionado IonIcons e colocado na home

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import { Home } from '../pages/home';
 import { Favorites } from '../pages/favorites';
+import { Ionicons } from '@expo/vector-icons';
 
 
 const Tab = createBottomTabNavigator();
@@ -19,8 +20,22 @@ export function Routes() {
         backgroundColor: "#fff"
       }
     }}>
-    <Tab.Screen name="Receitas do Sat" component={Home} />
-    <Tab.Screen name="Minhas Favoritas" component={Favorites} />
+    <Tab.Screen 
+    name="Receitas do Sat" 
+    component={Home} 
+    options={{ 
+      tabBarIcon: ({ color, size, focused }) => {
+        if(focused){
+          return <Ionicons name="home" color="#000000" size={size}/>
+        }
+        return <Ionicons name="home-outline" color={color} size={size}/>
+      }
+     }}
+    />
+    <Tab.Screen 
+    name="Minhas Favoritas" 
+    component={Favorites} 
+    />
   </Tab.Navigator>
   )
 }
