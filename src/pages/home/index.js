50d7c8a7afe27ab1f07f02800825d23f3cf85f1e
Johnny <johnny_s.o@hotmail.com>
2023-04-04 19:09:52 -0300

    Adicionado padding e color no background do home

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,15 +1,19 @@
-import { View, Text, StyleSheet} from 'react-native';
+import { View, Text, StyleSheet, SafeAreaView} from 'react-native';
 
 export function Home() {
   return(
-  <View style={styles.container}>
-    <Text>Página Home!</Text>
-  </View>
+  <SafeAreaView style={styles.container}>
+    <Text>Página Home</Text>
+  </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: 'red',
+    flex: 1,
+    backgroundColor: '#F3F9FF',
+    paddingTop: 36,
+    paddingStart: 14,
+    paddingEnd: 14,
   }
 })
