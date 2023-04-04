fb80068293fcf84b029242cadd2e8e1dafec8a97
Johnny <johnny_s.o@hotmail.com>
2023-04-04 19:10:13 -0300

    removida text

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -3,7 +3,7 @@ import { View, Text, StyleSheet, SafeAreaView} from 'react-native';
 export function Home() {
   return(
   <SafeAreaView style={styles.container}>
-    <Text>Página Home</Text>
+    
   </SafeAreaView>
   )
 }
