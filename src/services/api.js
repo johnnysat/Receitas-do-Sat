7b337c0c476c01345ca5d3aec67ae9f7189c3a12
Johnny <johnny_s.o@hotmail.com>
2023-04-06 12:16:02 -0300

    Configurando services/api com axios

---
+++ src/services/api.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+//Rodar com IPV4: json-server --watch -d 180 --host 192.168.15.96 db.json
+
+const api = axios.create({
+  baseURL: 'http://192.168.15.96:3000/foods'
+})
+
+export default api;
