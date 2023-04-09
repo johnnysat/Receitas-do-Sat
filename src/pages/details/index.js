b2c07f3c48a9183d923e0e2943e13ee5438023e9
Johnny <johnny_s.o@hotmail.com>
2023-04-08 22:22:00 -0300

    Ao clicar em algum card na foodlist, mostrará nome da receita como titulo

--- src/pages/details/index.js
+++ src/pages/details/index.js
@@ -1,8 +1,16 @@
+import { useLayoutEffect } from 'react';
 import { View, Text, StyleSheet} from 'react-native';
-import { useRoute } from '@react-navigation/native'
+import { useRoute, useNavigation } from '@react-navigation/native'
 
 export function Detail() {
   const route= useRoute() 
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: route.params?.data ? route.params?.data.name : "Detalhes da Receita"
+    })
+  }, [navigation, route.params?.data])
 
   return(
   <View style={styles.container}>
