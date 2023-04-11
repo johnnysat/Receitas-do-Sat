562ca965dec8423ea6fb50d89f037daabe4a9d17
Johnny <johnny_s.o@hotmail.com>
2023-04-11 17:42:33 -0300

    Estilizando container do ingrediente

--- src/components/ingredients/index.js
+++ src/components/ingredients/index.js
@@ -1,9 +1,22 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 
 export function Ingredients() {
   return(
-    <View>
+    <View style={styles.container}>
       <Text>Massa de Pizza</Text>
+      <Text>100mg</Text>
+
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#FFF',
+    marginBottom: 14,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 12,
+    borderRadius: 4,
+  }
+})
