8bfa717678c8dcb82bddbe97080fff3dd6d3d150
Johnny da Silva Oliveira <johnny_s.o@hotmail.com>
2023-05-03 22:11:19 -0300

    Adicionado useState para verificar as receitas salvas e adicionada mensagem para quando não houver receitas salvas

--- src/pages/favorites/index.js
+++ src/pages/favorites/index.js
@@ -1,9 +1,16 @@
+import { useState, useEffect } from 'react';
 import { View, Text, StyleSheet, SafeAreaView} from 'react-native';
 
 export function Favorites() {
+  const [receipes, setReceipes] = useState([]);
+
   return(
   <SafeAreaView style={styles.container}>
     <Text style={styles.title}>Minhas Receitas Favoritas!</Text>
+
+    {receipes.length === 0 && (
+      <Text>Você ainda não salvou nenhuma receita...</Text>
+    )}
   </SafeAreaView>
   )
 }
