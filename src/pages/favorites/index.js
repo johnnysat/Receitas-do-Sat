98fd85ce7ac5834c0b7a12d8cacf40dfb73e4740
Johnny da Silva Oliveira <johnny_s.o@hotmail.com>
2023-05-03 22:07:39 -0300

    Adicionando estilos ao container

--- src/pages/favorites/index.js
+++ src/pages/favorites/index.js
@@ -10,6 +10,11 @@ export function Favorites() {
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+    backgroundColor: '#f3f9ff',
+    paddingStart: 14,
+    paddingEnd: 14,
+    paddingTop: 36,
 
   }
 })
